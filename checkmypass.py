a715faece6be0638af863895458b8f37f88a3d4e
Marko Dankovich <dankovic.marko@yahoo.com>
2022-09-06 14:54:26 +0200

    first commit

---
+++ checkmypass.py
@@ -0,0 +1,33 @@
+from urllib import response
+import requests
+import hashlib
+
+
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching:{res.status_code}, check the API and try again')
+    else:
+        return res
+    
+
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':')for line in hashes.text)
+    for h in hashes:
+        print(h)
+
+
+
+def pwned_api_check(password):
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5],sha1password[5:]
+    response = request_api_data(first5_char)
+    print(first5_char, tail)
+    print(response)
+    return get_password_leaks_count(response, tail)
+
+pwned_api_check('123s')
+
+
